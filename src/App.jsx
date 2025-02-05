1362619d911488785ff1b1986d5a8da655fff864
Vansh Bhatia <133893287+Vanshprocoder@users.noreply.github.com>
2025-02-05 19:20:56 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import TimeTableUpload from './pages/TimeTableUpload.jsx';
 import LoginForm from './pages/Loginform.jsx';
+import ForgotPass from './pages/ForgotPass.jsx';
+import Otp from './pages/Otp.jsx';
 
 const App = () => {
   return (
@@ -10,6 +12,8 @@ const App = () => {
       <Routes>
         <Route path="/" element={<TimeTableUpload />} />
         <Route path="/login" element={<LoginForm />} />
+        <Route path="/forgotpass" element={<ForgotPass />} />
+        <Route path="/forgotpass/otp" element={<Otp />} />
        
       </Routes>
     </Router>
